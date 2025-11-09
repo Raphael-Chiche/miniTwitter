cae5598d9fb60790d8a317b4a828f85a703e149a
Raphael <raphael.ch2004@gmail.com>
2025-11-09 16:44:42 +0100

    fix: update API baseURL to use environment variable and add .env file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000"
-      : "https://twitterlike-backend.vercel.app",
+      : import.meta.env.VITE_API_URL,
 });
 
 
